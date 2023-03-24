0e73ba2a67b0ee6d5076d0444253f04764531204
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2023-03-24 18:19:45 +0300

    promise 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,10 @@
-export default function getResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
